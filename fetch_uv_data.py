5e7a59765c90ca960de4a2f981bbba8c6e8deff5
mehwishnaseem <smehwishnaseem@gmail.com>
2021-03-24 21:58:10 +0000

    Display data fetched from OpenUV on webpage

--- fetch_uv_data.py
+++ fetch_uv_data.py
@@ -2,16 +2,21 @@ import os
 import requests
 from pprint import pprint
 
+def fetch_uv_data():
+    uv_url_template ='https://api.openuv.io/api/v1/uv?lat={lat}&lng={lng}&dt={data}'
+    my_latitude = '51.52369'
+    my_longitude = '-0.0395857' 
+    my_date = '2018-11'
+    headers = {'x-access-token': os.environ.get('OPENUV_TOKEN')}
 
-uv_url_template ='https://api.openuv.io/api/v1/uv?lat={lat}&lng={lng}&dt={data}'
-my_latitude = '51.52369' 
-my_longitude = '-0.0395857' 
-my_date = '2018-11'
-headers = {'x-access-token': os.environ.get('OPENUV_TOKEN')}
-uv_url = uv_url_template.format(lat=my_latitude, lng = my_longitude, data=my_date)
-resp = requests.get(uv_url, headers=headers)
-if resp.ok:
-    uv = resp.json()
-else:
-    print(resp.reason)
-pprint(uv)
+    uv_url = uv_url_template.format(lat=my_latitude, lng=my_longitude, data=my_date)
+    resp = requests.get(uv_url, headers=headers)
+    
+    if resp.ok:
+        return resp.json()
+    else:
+        return resp.reason
+
+if __name__ == '__main__':
+    uv = fetch_uv_data()
+    pprint(uv)
--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify, json
+from fetch_uv_data import fetch_uv_data
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    html = "<h1>Hello {name}!</h1>".format(name="Mehwish") 
+    html = "<h1>Hello from {name}!</h1>\nGoto /api to fetch UV data".format(name="Mehwish") 
     return html
 
+@app.route("/api", methods=['GET'])
+def get_uv_data():
+    uv = fetch_uv_data()
+    return jsonify(uv)
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80, debug=True)
